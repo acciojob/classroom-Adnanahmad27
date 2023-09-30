8a13647f210a832dd849a820a4701f187a659c91
adnan <adnan232462@gmail.com>
2023-09-30 22:22:29 +0530

    first

---
+++ src/main/java/com/driver/StudentRepository.java
@@ -0,0 +1,65 @@
+package com.driver;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Repository
+public class StudentRepository {
+    HashMap<String,Student> studentHashMap = new HashMap<>();
+    HashMap<String,Teacher> teacherHashMap = new HashMap<>();
+    HashMap<String, List<String>> pairHashMap = new HashMap<>();
+
+    public void addStudent(Student student){
+        studentHashMap.put(student.getName() , student);
+    }
+    public void addTeacher(Teacher teacher){
+        teacherHashMap.put(teacher.getName() , teacher);
+    }
+    public void addStudentTeacherPair(String student,String teacher){
+        List<String> list;
+        if (pairHashMap.containsKey(teacher)) {
+            list=pairHashMap.get(teacher);
+        }else{
+            list = new ArrayList<>();
+            //list.add(teacher);
+        }
+        list.add(student);
+        pairHashMap.put(teacher,list);
+    }
+    public List<Student> getAllStudents(){
+        List<Student> list = new ArrayList<>();
+        for (Student student : studentHashMap.values()) {
+            list.add(student);
+        }
+        return list;
+    }
+    public List<Teacher> getAllTeachers(){
+        List<Teacher> list = new ArrayList<>();
+        for (Teacher teacher : teacherHashMap.values()) {
+            list.add(teacher);
+        }
+        return list;
+    }
+    public List<String> getAllStudentTeacherPairs(String name){
+        //List<String> list = new ArrayList<>();
+        for (List<String> strings : pairHashMap.values()) {
+            if(pairHashMap.containsKey(name)){
+                return pairHashMap.get(name);
+            }
+        }
+        return null;
+    }
+
+    public void deleteTeacherByName(String teacher) {
+        for(String key : teacherHashMap.keySet()){
+            if(teacherHashMap.containsKey(teacher)){
+                teacherHashMap.remove(key);
+            }
+        }
+    }
+
+}
---
+++ src/main/java/com/driver/StudentService.java
@@ -0,0 +1,60 @@
+package com.driver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    StudentRepository studentRepository;
+    public void addStudent(Student student){
+        studentRepository.addStudent(student);
+    }
+    public void addTeacher(Teacher teacher){
+        studentRepository.addTeacher(teacher);
+    }
+    public void addStudentTeacherPair(String student,String teacher){
+        studentRepository.addStudentTeacherPair(student,teacher);
+        List<Teacher> list = studentRepository.getAllTeachers();
+
+    }
+    public Student getStudentByName(String name) {
+        List<Student> list = studentRepository.getAllStudents();
+        for(Student s : list){
+            if(s.getName().equals(name)){
+                return s;
+            }
+        }
+        return null;
+    }
+    public Teacher getTeacherByName(String name) {
+        List<Teacher> list = studentRepository.getAllTeachers();
+        for(Teacher t: list){
+            if(t.getName().equals(name)){
+                return t;
+            }
+        }
+        return null;
+    }
+    public List<String> getStudentList(String name){
+        return studentRepository.getAllStudentTeacherPairs(name);
+    }
+    public List<String> getAllStudents(){
+        List<Student> list = studentRepository.getAllStudents();
+        List<String> studentList = new ArrayList<>();
+        for(Student student : list){
+            studentList.add(student.getName());
+        }
+        return studentList;
+    }
+    public void deleteTeacherByName(String teacher) {
+        studentRepository.deleteTeacherByName(teacher);
+    }
+}
